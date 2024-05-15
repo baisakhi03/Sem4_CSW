cc099fa2303cce932f9378efece8eae470726269
baisakhi03 <129980981+baisakhi03@users.noreply.github.com>
2024-05-15 22:43:12 +0530

    Update q8.java

--- Assignment_8/src/q8.java
+++ Assignment_8/src/q8.java
@@ -51,11 +51,9 @@ public class q8 {
 
         // Add edges
         graph.addEdge(0, 1);
-        graph.addEdge(0, 4);
+        graph.addEdge(0, 3);
         graph.addEdge(1, 2);
-        graph.addEdge(1, 3);
-        graph.addEdge(1, 4);
-        graph.addEdge(2, 3);
+        graph.addEdge(2, 4);
         graph.addEdge(3, 4);
 
         // Display adjacency matrix
