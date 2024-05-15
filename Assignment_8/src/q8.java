4419d953f27099f982727d2b34a1b132cbd33f6f
baisakhi03 <129980981+baisakhi03@users.noreply.github.com>
2024-05-15 22:51:37 +0530

    Update q8.java

--- Assignment_8/src/q8.java
+++ Assignment_8/src/q8.java
@@ -2,31 +2,31 @@
 import java.util.*;
 
 public class q8 {
-    private int V; // Number of vertices
+    private int Vertices; // Number of vertices
     private LinkedList<Integer>[] adjList; // Adjacency list
     private int[][] adjMatrix; // Adjacency matrix
 
-    public q8(int v) {
-        V = v;
-        adjList = new LinkedList[v];
-        adjMatrix = new int[v][v];
-        for (int i = 0; i < v; ++i) {
+    public q8(int vertices) {
+        Vertices = vertices;
+        adjList = new LinkedList[vertices];
+        adjMatrix = new int[vertices][vertices];
+        for (int i = 0; i < vertices; ++i) {
             adjList[i] = new LinkedList<>();
             Arrays.fill(adjMatrix[i], 0);
         }
     }
 
     // Add an edge to the graph
-    public void addEdge(int v, int w) {
-        adjList[v].add(w);
-        adjMatrix[v][w] = 1;
+    public void addEdge(int vertices, int w) {
+        adjList[vertices].add(w);
+        adjMatrix[vertices][w] = 1;
     }
 
     // Display adjacency matrix
     public void displayAdjMatrix() {
         System.out.println("Adjacency Matrix:");
-        for (int i = 0; i < V; ++i) {
-            for (int j = 0; j < V; ++j) {
+        for (int i = 0; i < Vertices; ++i) {
+            for (int j = 0; j < Vertices; ++j) {
                 System.out.print(adjMatrix[i][j] + " ");
             }
             System.out.println();
@@ -36,7 +36,7 @@ public class q8 {
     // Display adjacency list
     public void displayAdjList() {
         System.out.println("Adjacency List:");
-        for (int i = 0; i < V; ++i) {
+        for (int i = 0; i < Vertices; ++i) {
             System.out.print(i + " -> ");
             }
             System.out.println();
