d9770e58649e20ac326ed52bac56323969e2ee7b
baisakhi03 <129980981+baisakhi03@users.noreply.github.com>
2024-05-16 12:32:17 +0530

    Update q5.java

--- Assignment_5/q5.java
+++ Assignment_5/q5.java
@@ -1,33 +1,26 @@
-public class q5 {
-    private int intValue;
-    private double doubleValue;
+public class Q5 {
+    private int Val;
+    private double Value;
 
-    public void initialize(int intValue, double doubleValue) {
-        this.intValue = intValue;
-        this.doubleValue = doubleValue;
+    public void initialize(int Val, double Value) {
+        this.Val = Val;
+        this.Value = Value;
     }
 
     public static void main(String[] args) {
-        Runtime runtime = Runtime.getRuntime();
+        Runtime runtm = Runtime.getRuntime();
+        Q5 ob1 = new Q5();
+        Q5 ob2 = new Q5();
+    
+        ob1.initialize(20, 50);
+        ob2.initialize(40,80);
+        ob1 = null;
+        ob2 = null;
 
-        // Create objects
-        q5 obj1 = new q5();
-        q5 obj2 = new q5();
-
-        // Initialize objects
-        obj1.initialize(10, 5.5);
-        obj2.initialize(20, 8.8);
-
-        // Make objects unreachable
-        obj1 = null;
-        obj2 = null;
-
-        // Request garbage collection
         System.gc();
-
-        // Print memory usage
-        long usedMemory = runtime.totalMemory() - runtime.freeMemory();
-        long totalMemory = runtime.totalMemory();
+        
+        long usedMemory = runtm.totalMemory() - runtm.freeMemory();
+        long totalMemory = runtm.totalMemory();
         System.out.println("Memory used: " + usedMemory + " bytes");
         System.out.println("Total memory: " + totalMemory + " bytes");
     }
