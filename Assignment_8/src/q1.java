53436eb8a4d458b554c19ed75cba78b4dce9bb7d
baisakhi03 <129980981+baisakhi03@users.noreply.github.com>
2024-05-16 12:38:26 +0530

    Update q1.java

--- Assignment_8/src/q1.java
+++ Assignment_8/src/q1.java
@@ -4,57 +4,57 @@ public class q1 {
 	q1 left;
 	q1 right;
 	public q1(int info){
-		this.info = info;
-		this.left = null;
-		this.right=null;
+	   this.info = info;
+	   this.left = null;
+	   this.right=null;
 	}
 	public void insert(int value) {
-		if(value <= info) {
-			if(left == null) {
-				left = new q1(value);
+	  if(value <= info) {
+		if(left == null) {
+			left = new q1(value);
 			}
-			else {
-				left.insert(value);
+		else {
+			left.insert(value);
 			}
-		}else {
-			if(right == null) {
-				right = new q1(value);
-			}else {
-				right.insert(value);
+		    }
+	  else {
+		if(right == null) {
+			right = new q1(value);
+			}
+		else {
+			right.insert(value);
 			}
 		}
 	}
 	public void preOrderTraversal() {
-		System.out.print(info +" ");
+	    System.out.print(info +" ");
 		if(left != null) {
-			left.preOrderTraversal();
+		    left.preOrderTraversal();
 		}
 		if(right != null) {
-			right.preOrderTraversal();
+		    right.preOrderTraversal();
 		}
 	}
 	public void inorderTraversal() {
-		if(left != null) {
-			left.inorderTraversal();
+	    if(left != null) {
+		  left.inorderTraversal();
 		}
-		System.out.print(info+" ");
 		if(right != null) {
-			right.inorderTraversal();
+		    right.inorderTraversal();
 		}
 	}
-	public void postOrdertraversal() {
-		if(left != null) {
-			left.preOrderTraversal();
+	  public void postOrdertraversal() {
+	      if(left != null) {
+		   left.preOrderTraversal();
 		}
 		if(right != null) {
-			right.preOrderTraversal();
+		   right.preOrderTraversal();
 		}
 		System.out.print(info+" ");
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 		q1 root = new q1(7);
 		root.insert(3);
 		root.insert(17);
